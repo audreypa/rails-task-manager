f4af8e9e8e21cb2cc7907bca18a1818627c82f55
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-13 14:14:20 +0200

    refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create', as: 'create_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
   # Defines the root path route ("/")
+  root to: 'tasks#index'
   # root "articles#index"
 end
